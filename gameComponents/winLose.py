3a4099ac4661a8055ca16be3472fff275f8ca874
Lulopera <71723463+lulopera@users.noreply.github.com>
2020-11-28 13:36:31 -0500

    last minute corrections

--- gameComponents/winLose.py
+++ gameComponents/winLose.py
@@ -13,24 +13,28 @@ def winorlose(status):
 	print(pre_message + "Would you like to play again?")
 	choice = input("Y / N? ")
 
-	if choice == "Y" or choice == "y":
+	if choice == "N" or choice == "n":
+		
+		print("You chose to quit. Best of luck next time!")
+		exit()
+
+	elif choice == "Y" or choice == "y":
 
 		gameVars.player_lives = 5
 		gameVars.computer_lives = 5
 		gameVars.player = False
-
-	elif choice == "N" or choice == "n":
-		
-		print("You chose to quit. Best of luck next time!")
-		exit()
 	
 	else:
 		print("Make a valid choice - Y or N")
 		choice = input("Y / N")
 
 
-		if choice == "Y" or choice == "y":
+	if choice == "Y" or choice == "y":
 
-			gameVars.player_lives = 5
-			gameVars.computer_lives = 5
-			gameVars.player = False
+		gameVars.player_lives = 5
+		gameVars.computer_lives = 5
+		gameVars.player = False
+
+	else:
+		print("You chose to quit. Best of luck next time!")
+		exit()
